eb5012abfa4ecdf7a6cbaf1ce927865e2d8af3b2
nguyenduynien <42843173+nguyenduynien@users.noreply.github.com>
2021-05-29 18:53:09 +0700

    Row and Column
    Update

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,223 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My first app'),
-        centerTitle: true,
-        backgroundColor: Colors.pink[100],
+        title: Text('Home'),
+        leading: Icon(Icons.home),
+        backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Text(
-            'hey man!',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.black,
-              fontFamily: 'OpenSans',
-            )
+      body: Container(
+        //padding: EdgeInsets.all(5),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.all(10),
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text(
+                        "Product1.1",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        )
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.all(10),
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text(
+                        "Product1.2",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        )
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.all(10),
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text(
+                        "Product1.3",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        )
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.all(10),
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text(
+                        "Product1.4",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        )
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.all(10),
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text(
+                        "Product2.1",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        )
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.all(10),
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text(
+                        "Product2.2",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        )
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.all(10),
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text(
+                        "Product2.3",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        )
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.all(10),
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text(
+                        "Product2.4",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        )
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.all(10),
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text(
+                        "Product3.1",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        )
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.all(10),
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text(
+                        "Product3.2",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        )
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.all(10),
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text(
+                        "Product3.3",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        )
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: EdgeInsets.all(10),
+                  color: Colors.orange,
+                  child: Center(
+                    child: Text(
+                        "Product3.4",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        )
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('click'),
-        backgroundColor: Colors.pink[100],
       ),
     );
   }
